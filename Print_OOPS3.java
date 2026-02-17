860ab4decbbe38789cb57ed300fe9df64164cf91
Abib-2007 <ak0161@srmist.edu.in>
2026-02-17 09:53:54 +0530

    Rename class to Print_OOPS3 and update main method

--- Print_OOPS3.java
+++ Print_OOPS3.java
@@ -1,5 +1,5 @@
 
-        public class STEPBannerApp3 {
+        public class Print_OOPS3 {
       public static void main(String[] args){
 	
 		    System.out.println("...");		   
@@ -12,4 +12,5 @@
 			System.out.println(String.join(" ","   ***   " , " ", "   ***   " , " ", " **      " , " ", "  *****  "));
     }
 }
+
 
